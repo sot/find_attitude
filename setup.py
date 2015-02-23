2811e18e2f306d5a5f2db8da93d26c27d303f882
Tom Aldcroft <taldcroft@gmail.com>
2015-02-23 15:39:06 -0500

    Make into a proper package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='find_attitude',
+      version='0.1',
+      description='Find attitude given a list of ACA star yag/zag coordinates',
+      author='Tom Aldcroft',
+      author_email='taldcroft@cfa.harvard.edu',
+      packages=['find_attitude'],
+      license='BSD',
+      )
