53c819f3b116985a445a5471b91b1026e89204da
Jean Connelly <jconnelly@cfa.harvard.edu>
2017-05-18 14:06:17 -0400

    Increment version to 0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='find_attitude',
-      version='0.1',
+      version='0.2',
       description='Find attitude given a list of ACA star yag/zag coordinates',
       author='Tom Aldcroft',
       author_email='taldcroft@cfa.harvard.edu',
