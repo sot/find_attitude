14c64dcbc30df7883d79ee38069062eb28effac0
Javier Gonzalez <javier.gonzalez@cfa.harvard.edu>
2020-09-15 19:03:57 -0400

    using ska-theme (fixup)

--- docs/conf.py
+++ docs/conf.py
@@ -95,9 +95,9 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 html_theme = 'bootstrap-ska'
 html_theme_options = {
-logotext1: 'Ska!' ,
-logotext2: 'find_attitude',
-logotext3: '',
+    'logotext1': 'Ska!' ,
+    'logotext2': 'find_attitude',
+    'logotext3': '',
 }
 
 # Theme options are theme-specific and customize the look and feel of a theme
