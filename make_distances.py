72ab487ec7f2ac4877a1b63832b43de2caa19ec9
Tom Aldcroft <taldcroft@gmail.com>
2024-07-12 08:50:09 -0700

    Revert max_mag param in favor of constant

--- make_distances.py
+++ make_distances.py
@@ -12,8 +12,9 @@ import tqdm
 from astropy.table import Table, vstack
 
 NSIDE = 16
-MIN_ACA_DIST = 25.0 / 3600  # 25 arcmin min separation
+MIN_ACA_DIST = 25.0 / 3600  # 25 arcsec min separation
 MAX_ACA_DIST = 2.1  # degrees corner to corner
+MAX_MAG = 10.5  # Max mag to include in the distances file
 
 # Define a global cache for star data which persists in ipython for re-run of module.
 if "STAR_CACHE" not in globals():
@@ -29,18 +30,17 @@ MICROAGASC_FILE = f"microagasc_{AGASC_VERSION}.fits"
 
 
 def make_distances_h5(
-    max_mag=10.5,
     outfile=DISTANCES_FILE,
     microagasc_file=MICROAGASC_FILE,
 ):
     if os.path.exists(outfile):
         raise IOError("{} already exists".format(outfile))
-    agasc = get_microagasc(microagasc_file, max_mag=max_mag)
+    agasc = get_microagasc(microagasc_file)
     dists = get_all_dists(agasc)
     make_h5_file(dists, outfile)
 
 
-def make_microagasc(max_mag=10.5, outfile=MICROAGASC_FILE):
+def make_microagasc(outfile=MICROAGASC_FILE):
     path = agasc.get_agasc_filename("miniagasc_*", version=AGASC_VERSION)
     if path.exists():
         return
@@ -48,7 +48,7 @@ def make_microagasc(max_mag=10.5, outfile=MICROAGASC_FILE):
     print(f"Reading miniagasc {path}")
     with tables.open_file(path) as h5:
         mag_aca = h5.root.data.col("MAG_ACA")
-        ok = mag_aca < max_mag
+        ok = mag_aca < MAX_MAG
         mag_aca = mag_aca[ok]
         ra = h5.root.data.col("RA")[ok]
         ra_pm = h5.root.data.col("PM_RA")[ok]
@@ -65,11 +65,11 @@ def make_microagasc(max_mag=10.5, outfile=MICROAGASC_FILE):
     dat.write(outfile, overwrite=True)
 
 
-def get_microagasc(filename=MICROAGASC_FILE, date="2025:001", max_mag=10.5):
+def get_microagasc(filename=MICROAGASC_FILE, date="2025:001"):
     if not os.path.exists(filename):
         make_microagasc()
     stars = Table.read(filename, format="fits")
-    stars = stars[stars["MAG_ACA"] < max_mag]
+    stars = stars[stars["MAG_ACA"] < MAX_MAG]
 
     agasc.agasc.add_pmcorr_columns(stars, date=date)
 
@@ -121,6 +121,11 @@ def _get_dists(s0, s1):
 
 def get_dists_for_region(stars, ipix):
     s0 = get_stars_for_region(stars, ipix)
+
+    # Get the index of eight potential neighbors. Note that some pixels may have only
+    # seven valid neighbors, with the missing neighbor having an index of -1. This is
+    # handled correctly by the get_stars_for_region function, which will return an
+    # empty table for the missing neighbor.
     ipix_neighbors = hp.neighbours(ipix, nside=NSIDE)
     s1s = [
         get_stars_for_region(stars, ipix_neighbor) for ipix_neighbor in ipix_neighbors
@@ -140,7 +145,7 @@ def make_h5_file(t, filename=DISTANCES_FILE):
         h5.root.data.flush()
 
     with tables.open_file(filename, mode="a", filters=filters) as h5:
-        print("Creating index")
+        print("Creating index (takes a few minutes)")
         h5.root.data.cols.dists.create_index()
         h5.flush()
 
